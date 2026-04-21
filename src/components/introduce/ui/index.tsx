d366f94b371695589581c04011e33d8b72994b22
이현빈 <lhb0107@naver.com>
2026-04-22 08:43:04 +0900

    feat(belowImageText): 소개페이지 히어로이미지 아래 text 구현

--- src/components/introduce/ui/index.tsx
+++ src/components/introduce/ui/index.tsx
@@ -1,15 +1,19 @@
 import Image from "next/image";
+import BelowHeroImageText from "@/components/introduce/ui/belowHeroImageText";
 
 export default function IntroducePage() {
   return (
-    <section className="relative h-[33.75rem] w-full">
-      <Image
-        src={"/image/introduce/introduceHeroImage.png"}
-        alt={"introduceHeroImage"}
-        fill
-        sizes="100vw"
-        className="object-cover"
-      />
+    <section className="w-full">
+      <div className="relative h-[33.75rem] w-full">
+        <Image
+          src={"/image/introduce/introduceHeroImage.png"}
+          alt={"introduceHeroImage"}
+          fill
+          sizes="100vw"
+          className="object-cover"
+        />
+      </div>
+      <BelowHeroImageText />
     </section>
   );
 }
