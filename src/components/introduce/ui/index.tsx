e58af73ec51e6a940e46cb0bb0310fc1fb311bc9
이현빈 <lhb0107@naver.com>
2026-04-22 09:40:13 +0900

    fix(introduce): hero이미지 잘리는 문제 수정

--- src/components/introduce/ui/index.tsx
+++ src/components/introduce/ui/index.tsx
@@ -6,15 +6,14 @@ import IntroduceWordBanner from "@/components/introduce/ui/introduceWordBanner";
 export default function IntroducePage() {
   return (
     <section className="w-full pb-[9rem]">
-      <div className="relative h-[33.75rem] w-full">
-        <Image
-          src={"/image/introduce/introduceHeroImage.png"}
-          alt={"introduceHeroImage"}
-          fill
-          sizes="100vw"
-          className="object-cover"
-        />
-      </div>
+      <Image
+        src={"/image/introduce/introduceHeroImage.png"}
+        alt={"introduceHeroImage"}
+        width={4324}
+        height={2164}
+        sizes="100vw"
+        className="h-auto w-full"
+      />
       <BelowHeroImageText />
       <IntroduceWordBanner />
       <CompanyHistory />
