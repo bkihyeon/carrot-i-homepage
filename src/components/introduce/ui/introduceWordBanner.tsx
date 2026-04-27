4e3f58cb90957f7f10d5a6c0fbf1817c9c0468a1
이현빈 <lhb0107@naver.com>
2026-04-27 14:57:07 +0900

    fix(introduce): wordBanner 반응형 적용 x -> o

--- src/components/introduce/ui/introduceWordBanner.tsx
+++ src/components/introduce/ui/introduceWordBanner.tsx
@@ -6,7 +6,7 @@ export default function IntroduceWordBanner() {
       }
     >
       <div className={""}>
-        <p className={"type-heading-4 tablet:type-heading-2"}>
+        <p className="type-heading-4 tablet-type-heading-2">
           작은 시작에서 AI 데이터 플로우까지 <br /> 데이터로 만들어온 성장의
           흐름
         </p>
